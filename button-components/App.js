7fc4ff54fb43d65bdf2750d0a3ab1e17b7840aed
JEROME Emilien <jerome.emi@onja.org>
2020-10-09 16:33:21 +0300

    added react-buttons component

--- button-components/App.js
+++ button-components/App.js
@@ -7,97 +7,91 @@ function App() {
         <div>
             <h1>Buttons</h1>
             <div className="first-components">
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button />",
-                    nameClass: 'button',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button &:hover, &:focus/>",
-                    nameClass: 'button hover',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button variant='outline'/>",
-                    nameClass: 'button outline',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button &:hover, &:focus/>",
-                    nameClass: 'button outline outlineHover',
-                }}/>
-
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button variant='text'/>",
-                    nameClass: 'button text',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button &:hover, &:focus/>",
-                    nameClass: 'button text textHover',
-                }}/>
+                <Button
+                    textContent= "Default" 
+                    elementExample= "<Button &:hover, &:focus/>"
+                    hoverDefault= 'hover'
+                />
+                <Button
+                    textContent= "Default" 
+                    elementExample= "<Button &:hover, &:focus/>"
+                    variant= 'outline'
+                />
+                <Button
+                    textContent= "Default" 
+                    elementExample= "<Button &:hover, &:focus/>"
+                    variant= 'text'
+                />
             </div>
             <div className="second-components">
                 <Button 
-                    button = {{
-                        textContent: "Default", 
-                        elementExample: "<Button disableShadow/>",
-                        nameClass: 'button disableShadow',
-                    }}/>
+                        textContent= "Default" 
+                        elementExample= "<Button disableShadow/>"
+                        disableShadow
+                    />
                     <Button 
-                    button = {{
-                        textContent: "Default", 
-                        elementExample: "<Button disable/>",
-                        nameClass: 'button disable',
-                    }}/>
+                        textContent= "Disabled" 
+                        elementExample= "<Button disable/>"
+                        disable
+                    />
                     <Button 
-                    button = {{
-                        textContent: "Default", 
-                        elementExample: "<Button variant='text' disable/>",
-                        nameClass: 'button textDisable',
-                    }}/>
+                        textContent= "Disabled" 
+                        elementExample= "<Button variant='text' disable/>"
+                        variant= 'text'
+                        disable
+                    />
             </div>
             <div className="third-components">
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button startIcon='Local_grocery_store'/>",
-                    nameClass: 'button icon iconStart',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button endIcon='Local_grocery_store'/>",
-                    nameClass: 'button icon iconEnd',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button />",
-                    nameClass: 'button',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button />",
-                    nameClass: 'button',
-                }}/>
-                <Button 
-                button = {{
-                    textContent: "Default", 
-                    elementExample: "<Button />",
-                    nameClass: 'button',
-                }}/>
+                <Button
+                    textContent= "Default" 
+                    elementExample= "<Button startIcon='Local_grocery_store'/>"
+                    startIcon
+                />
+                <Button
+                    textContent= "Default" 
+                    elementExample= "<Button endIcon='Local_grocery_store'/>"
+                    endIcon
+                />
             </div>
-            
+            <div className="fourth-components">
+                <Button 
+                        textContent= "Default" 
+                        elementExample= "<Button size='sm'/>"
+                        size='sm'
+                    />
+                    <Button 
+                        textContent= "Default" 
+                        elementExample= "<Button size='md'/>"
+                        size='md'
+                    />
+                    <Button 
+                        textContent= "Default" 
+                        elementExample= "<Button size='lg'/>"
+                        size='lg'
+                    />
+                </div>
+                <div className="fifth-components">
+                <Button 
+                        textContent= "Default" 
+                        elementExample= "<Button &:hover, &:focus/>"
+                        color='default'
+                    />
+                    <Button 
+                        textContent= "Default" 
+                        elementExample= "<Button &:hover, &:focus/>"
+                        color='primary'
+                    />
+                    <Button 
+                        textContent= "Secondary" 
+                        elementExample= "<Button &:hover, &:focus/>"
+                        color='secondary'
+                    />
+                    <Button 
+                        textContent= "Danger" 
+                        elementExample= "<Button &:hover, &:focus/>"
+                        color='danger'
+                    />
+                </div>
         </div>
         )
 }
--- button-components/Button.js
+++ button-components/Button.js
@@ -3,16 +3,73 @@ import 'style.css'
 
 function Button(props) {
     // function icon() {
-    //     if(props.button.nameClass === 'button icon iconStart') {
+    //     if(props.nameClass === 'button icon iconStart') {
     //         return <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>;
     //         // console.log(svg);
     //     }
     // }
-    
+    let nameOfClass;
+
+    if(props.variant === 'outline') {
+        nameOfClass = 'button outline outlineHover'
+    }
+
+    if(props.variant === 'text') {
+        nameOfClass = 'button text textHover'
+    }
+
+    if(props.hoverDefault === 'hover') {
+        nameOfClass = 'button hover'
+    }
+
+    if(props.disableShadow) {
+        nameOfClass = 'button disableShadow'
+    }
+
+    if(props.disable) {
+        nameOfClass = 'button disable'
+    }
+
+    if(props.variant === "text" && props.disable) {
+        nameOfClass = 'button textDisable'
+    }
+
+    if(props.startIcon) {
+        nameOfClass = 'button icon iconStart'
+    }
+
+    if(props.endIcon) {
+        nameOfClass = 'button icon iconEnd'
+    }
+
+    if(props.size === 'sm') {
+        nameOfClass = 'button size sizedSm'
+    }
+
+    if(props.size === 'md') {
+        nameOfClass = 'button size sizedMd'
+    }
+    if(props.size === 'lg') {
+        nameOfClass = 'button size sizedLg'
+    }
+    if(props.color === 'default') {
+        nameOfClass = 'button default'
+    }
+    if(props.color === 'primary') {
+        nameOfClass = 'button primary'
+    }
+    if(props.color === 'secondary') {
+        nameOfClass = 'button secondary'
+    }
+    if(props.color === 'danger') {
+        nameOfClass = 'button danger'
+    }
     return (
     <div>
-        <span>{props.button.elementExample}</span><br/>
-        <button className={props.button.nameClass}>{props.button.nameClass === 'button icon iconStart' ? <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg> : '' }{props.button.textContent}</button>
+        <span>{props.elementExample}</span><br/>
+        <button className={nameOfClass}>
+        {props.textContent}
+        </button>
     </div>
     )
    
