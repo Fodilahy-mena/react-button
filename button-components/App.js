83b76c5707f073cbcafc85f13e229261fbcf8b81
JEROME Emilien <jerome.emi@onja.org>
2020-10-09 10:39:02 +0300

    remove gitignore files

--- button-components/App.js
+++ button-components/App.js
@@ -2,28 +2,99 @@ import React from 'react'
 import Button from './Button'
 
 function App() {
-    // const DefaultStyle = {
-    //     background: '#E0E0E0',
-    //     boxShadow: '0px 2px 3px rgba(51, 51, 51, 0.2)',
-    //     borderRadius: '6px',
-    //     width: '81px',
-    //     height: '36px',
-    //     border: '0',
     
-    // }
     return (
         <div>
             <h1>Buttons</h1>
-            <div>
+            <div className="first-components">
                 <Button 
                 button = {{
                     textContent: "Default", 
                     elementExample: "<Button />",
-                    name: 'button',
+                    nameClass: 'button',
                 }}/>
                 <Button 
                 button = {{
                     textContent: "Default", 
+                    elementExample: "<Button &:hover, &:focus/>",
+                    nameClass: 'button hover',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button variant='outline'/>",
+                    nameClass: 'button outline',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button &:hover, &:focus/>",
+                    nameClass: 'button outline outlineHover',
+                }}/>
+
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button variant='text'/>",
+                    nameClass: 'button text',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button &:hover, &:focus/>",
+                    nameClass: 'button text textHover',
+                }}/>
+            </div>
+            <div className="second-components">
+                <Button 
+                    button = {{
+                        textContent: "Default", 
+                        elementExample: "<Button disableShadow/>",
+                        nameClass: 'button disableShadow',
+                    }}/>
+                    <Button 
+                    button = {{
+                        textContent: "Default", 
+                        elementExample: "<Button disable/>",
+                        nameClass: 'button disable',
+                    }}/>
+                    <Button 
+                    button = {{
+                        textContent: "Default", 
+                        elementExample: "<Button variant='text' disable/>",
+                        nameClass: 'button textDisable',
+                    }}/>
+            </div>
+            <div className="third-components">
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button startIcon='Local_grocery_store'/>",
+                    nameClass: 'button icon iconStart',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button endIcon='Local_grocery_store'/>",
+                    nameClass: 'button icon iconEnd',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button />",
+                    nameClass: 'button',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button />",
+                    nameClass: 'button',
+                }}/>
+                <Button 
+                button = {{
+                    textContent: "Default", 
+                    elementExample: "<Button />",
+                    nameClass: 'button',
                 }}/>
             </div>
             
